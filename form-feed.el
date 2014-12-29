063eb4219d71c4d54807a32c381ebbd21b7a73af
Vasilij Schneidermann <v.schneidermann@gmail.com>
2014-12-29 14:15:59 +0100

    Bump version

--- form-feed.el
+++ form-feed.el
@@ -5,7 +5,7 @@
 ;; Author: Vasilij Schneidermann <v.schneidermann@gmail.com>
 ;; URL: https://github.com/wasamasa/form-feed
 ;; Keywords: faces
-;; Version: 0.1.1
+;; Version: 0.1.2
 
 ;; This file is NOT part of GNU Emacs.
 
